57c1caebaccc129e275027040b30093a336b3646
marios8543 <marios8543@gmail.com>
2020-04-03 22:21:01 +0300

    added support for mixed content libraries
    fixed setup.py
    check previous commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='Jellyfin-CLI',
-    version='1.4.1',
+    version='1.5',
     scripts=['jellyfin-cli'],
     author="marios8543",
     author_email="marios8543@gmail.com",
